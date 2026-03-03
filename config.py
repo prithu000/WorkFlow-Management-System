ae0b50597309336a346e0ca664923ecccd7943e0
prithu000 <prithumaheshwari@gmail.com>
2026-03-03 23:43:14 +0530

    Refactor email configuration to use environment variables

--- config.py
+++ config.py
@@ -5,10 +5,9 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = "sqlite:///company.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 587
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT", 587))
     MAIL_USE_TLS = True
-    MAIL_USERNAME =os.environ.get("MAIL_USERNAME")
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_USERNAME")
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
