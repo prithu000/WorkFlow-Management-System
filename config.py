da562147018a9eeae99b370439345a016b214d92
prithu000 <prithumaheshwari@gmail.com>
2026-03-03 23:45:33 +0530

    Update mail server settings in config.py
    Updated mail server configuration to use SendGrid with fixed values.

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = "sqlite:///company.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get("MAIL_SERVER")
-    MAIL_PORT = int(os.environ.get("MAIL_PORT", 587))
+    MAIL_SERVER = "smtp.sendgrid.net"
+    MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_USERNAME = "apikey"
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
+    MAIL_DEFAULT_SENDER = "rahul.business940@gmail.com"
